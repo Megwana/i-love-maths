5787b16069bb06b8b2b312acce1c35ef1e3a1116
Megan <megantech@hotmail.com>
2022-09-06 18:16:46 +0000

    add eventListeners

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        });
+    }
+    
 function runGame(){
     // creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random( * 25) + 1);
